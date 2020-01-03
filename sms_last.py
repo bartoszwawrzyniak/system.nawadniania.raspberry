540632159b5e5d98e56f09b9ac91892b529dd92d
Bartosz Wawrzyniak <44320797+bartoszwawrzyniak@users.noreply.github.com>
2020-01-03 23:41:06 +0100

    Update sms_last.py

--- sms_last.py
+++ sms_last.py
@@ -1,6 +1,6 @@
 import requests
 
-v_url='https://ssl.smsapi.pl/sms.do?username=dzial.it@tfp.com.pl&password=1d1a99f96b751938e01762377c4f8697&from=TFP IT&to=48603780613&message=exec(open('last_watered').read())'
+v_url='https://ssl.smsapi.pl/sms.do?username=username_sms_api&password=smsapi_md5&from=TFP IT&to=48603780613&message=exec(open('last_watered').read())'
 
 r = requests.get(v_url)
 
